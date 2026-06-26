4a23be3c4d91ff6f74a7a15e1400205fd68f6af5
devansh mamoriya <devanshmamoriya@gmail.com>
2026-06-26 11:26:50 +0530

    Moves Zero solution with TC:O(n) and SC:O(n) added

--- MoveZeros/MoveZeros2.java
+++ MoveZeros/MoveZeros2.java
@@ -4,6 +4,23 @@ public class MoveZeros2 {
     public static void main(String[] args) {
         int arr[] = {0, 1, 0, 3, 12};
         int n = arr.length;
+
+        int i=0;
+        for(int j=0; j<n; j++){
+            if(arr[j] != 0){
+                arr[i] = arr[j];
+                i++;
+            }
+        
+        while(i<n){
+            arr[i] = 0;
+            i++;
+        }
+        
+        for(int k=0; k<n; k++){
+            System.out.print(arr[k] + " ");
+        }
+        
         
     }
 }
